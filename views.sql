e68f0b499bab97dba60bcb753cc50c0bf4ce1187
LuisSimoes17 <luis.d.simoes@tecnico.ulisboa.pt>
2019-11-05 19:38:49 +0000

    Views with 0 errors

--- views.sql
+++ views.sql
@@ -1,28 +1,28 @@
 create view dim_date as
-	select distinct date_timestamp, extract (day from date_timestamp),
-		extract (month from date_timestamp), extract (year from date_timestamp)
-	from consultation;
+select distinct date_timestamp, extract(day from date_timestamp) "day",
+extract(month from date_timestamp) "month", extract(year from date_timestamp) "year"
+from consultation;
 	
 create view dim_client as
-	select VAT, gender, age
-	from client;
+select VAT, gender, age
+from client;
 	
 create view dim_location_client as
-	select distinct zip, city
-	from client;
+select distinct zip, city
+from client;
 	
 create view facts_consults as
-	select dc.VAT, dd.date_timestamp, dlc.zip,
-		count(procedure_in_consultation.name), count(prescription.name),
-		count(consultation_diagnostic.ID)
-	from dim_client dc, dim_date dd, dim_location_client dlc,
-		procedure_in_consultation, prescription, consultation_diagnostic,
-		appointment, client
-	where dc.VAT = appointment.VAT_client
-		and dc.VAT = client.VAT
-		and dlc.zip = client.zip
-		and dd.date_timestamp = appointment.date_timestamp
-		and dd.date_timestamp = procedure_in_consultation.date_timestamp
-		and dd.date_timestamp = prescription.date_timestamp
-		and dd.date_timestamp = consultation_diagnostic.date_timestamp
-	group by dc.VAT;
+select dc.VAT, dd.date_timestamp, dlc.zip,
+count(procedure_in_consultation.name), count(prescription.name),
+count(consultation_diagnostic.ID)
+from dim_client dc, dim_date dd, dim_location_client dlc,
+procedure_in_consultation, prescription, consultation_diagnostic,
+appointment, client
+where dc.VAT = appointment.VAT_client
+and dc.VAT = client.VAT
+and dlc.zip = client.zip
+and dd.date_timestamp = appointment.date_timestamp
+and dd.date_timestamp = procedure_in_consultation.date_timestamp
+and dd.date_timestamp = prescription.date_timestamp
+and dd.date_timestamp = consultation_diagnostic.date_timestamp
+group by dc.VAT;
