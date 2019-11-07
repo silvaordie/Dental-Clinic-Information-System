a94ac58fed5bc9080874e902d91217bd8171a09c
LuisSimoes17 <luis.d.simoes@tecnico.ulisboa.pt>
2019-11-07 14:34:40 +0000

    Views done!

--- views.sql
+++ views.sql
@@ -5,7 +5,7 @@ drop view facts_consults;
 
 
 create view dim_date as
-select distinct date_timestamp, extract(day from date_timestamp) "day",
+select distinct date_timestamp "date", extract(day from date_timestamp) "day",
 extract(month from date_timestamp) "month", extract(year from date_timestamp) "year"
 from consultation;
 	
@@ -19,8 +19,8 @@ from client;
 	
 create view facts_consults as
 select dc.VAT, dd.date_timestamp, dlc.zip,
-count(procedure_in_consultation.name), count(prescription.name),
-count(consultation_diagnostic.ID)
+count(distinct procedure_in_consultation.name), count(distinct prescription.name),
+count(distinct consultation_diagnostic.ID)
 from dim_client dc, dim_date dd, dim_location_client dlc,
 procedure_in_consultation, prescription, consultation_diagnostic,
 appointment, client
@@ -36,14 +36,3 @@ and appointment.VAT_doctor = prescription.VAT_doctor
 and appointment.VAT_doctor = consultation_diagnostic.VAT_doctor
 and prescription.ID = consultation_diagnostic.ID
 group by dc.VAT, dd.date_timestamp;
-
-create view facts_consults as
-select distinct dc.VAT, dd.date_timestamp, dlc.zip
-from dim_client dc, dim_date dd, dim_location_client dlc,
-appointment, client, consultation
-where dc.VAT = appointment.VAT_client
-and appointment.VAT_client = client.VAT
-and dlc.zip = client.zip
-and dd.date_timestamp = appointment.date_timestamp
-and appointment.date_timestamp = consultation.date_timestamp
-and appointment.VAT_doctor = consultation.VAT_doctor;
