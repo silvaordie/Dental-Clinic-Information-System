b61f43bc090d43184b3326e5d83d2e7cec5c3c23
Pedro Ferreira <pf.pedroferreira@gmail.com>
2019-11-07 15:42:43 +0000

    indexes done

--- indexes.sql
+++ indexes.sql
@@ -1,1 +1,6 @@
 
+create index id_1 on consultation (VAT_doctor) using hash;
+
+create index id_score on supervision_report (evaluation) using BTREE;
+
+create fulltext index idx on supervision_report(description);
